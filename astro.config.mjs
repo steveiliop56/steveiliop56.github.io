343deb5e081afa0912eea555154d5b716ac4e653
Stavros <steveiliop56@gmail.com>
2024-12-31 21:35:24 +0000

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.VITE_SITE_URL,
   base: "/",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro"
