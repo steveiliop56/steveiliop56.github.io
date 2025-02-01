e577f7a19817ec4d3e195bba66cb7ce1a5bba0f6
Stavros <steveiliop56@gmail.com>
2025-02-01 19:22:22 +0200

    feat: add animations to the mobile menu

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 0.25s ease-in-out",
+        "fade-out": "fadeOut 0.25s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
   darkMode: "class",
