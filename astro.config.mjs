2814555df0ed273173a034f901e1aa4c632a570d
Stavros <steveiliop56@gmail.com>
2025-12-18 16:58:33 +0200

    fix: fix tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import { remarkAlert } from "remark-github-blockquote-alert";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://doesmycode.work",
   base: "/",
-  integrations: [tailwind(), icon(), sitemap()],
+  integrations: [icon(), sitemap()],
   markdown: {
     shikiConfig: { theme: "one-dark-pro" },
     remarkPlugins: [remarkAlert],
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
