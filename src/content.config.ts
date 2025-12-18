9c019c3537d7fb8ff05472179d5943ded22327d9
Stavros <steveiliop56@gmail.com>
2025-12-18 16:49:18 +0200

    chore: apply changes for astro v5

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const posts = defineCollection({
+  loader: glob({ pattern: "*.md", base: "./src/content/posts" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    publishedOn: z.coerce.date(),
+    updatedOn: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = { posts };
