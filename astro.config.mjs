67cb4b97f4229b03418e5a72b96fcf83f86f3436
Stavros <steveiliop56@gmail.com>
2026-05-14 16:50:08 +0300

    feat: post page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
+import { remarkAlert } from "remark-github-blockquote-alert";
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -9,6 +9,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  markdown: {
+    shikiConfig: { theme: "one-dark-pro" },
+    remarkPlugins: [remarkAlert],
+  },
   integrations: [icon()],
 });
