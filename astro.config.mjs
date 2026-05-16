9ec410f72e033e2a146c46672cc5a3256d903f88
Stavros <steveiliop56@gmail.com>
2026-05-16 22:32:38 +0300

    feat: rss and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import { remarkAlert } from "remark-github-blockquote-alert";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://doesmycode.work",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
@@ -13,5 +16,5 @@ export default defineConfig({
     shikiConfig: { theme: "one-dark-pro" },
     remarkPlugins: [remarkAlert],
   },
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 });
